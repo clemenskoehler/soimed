448d7e5f1ffcd562b47cb579ab9648b3181cfeff
Clemens Köhler <clemens.koehler@rwth-aachen.de>
2025-04-09 11:10:32 +0200

    First Drawing

--- widget.cpp
+++ widget.cpp
@@ -16,5 +16,15 @@ Widget::~Widget()
 
 void Widget::MalePixel()
 {
-    ui->pushButton_Pixel->setText("Funktioniert");
+    // Erzeuge ein Objekt vom Typ QImage
+    QImage image(512,512, QImage::Format_RGB32);
+
+    image.fill(qRgb(0,0,0));
+
+    // Schreibe einen roten Pixel
+    image.setPixel(50, 100, qRgb(255, 0, 0));
+
+    // Bild auf Benutzeroberflaeche anzeigen
+    ui->label_image->setPixmap(QPixmap::fromImage(image));
+    repaint();
 }
